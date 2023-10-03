2e2d65e1d5d81140b908df445212b8ab7cb25e83
Richard <richard.h.passos@hotmail.com>
2023-10-02 23:06:47 -0300

    fix fixture comments

--- tests/fixtures/transactions.js
+++ tests/fixtures/transactions.js
@@ -25,14 +25,14 @@ export const transactions = [
     totalCost: 1254.0,
   },
   {
-    institutionId: 'c1daef5f-4bd0-4616-bb62-794e9b5d8ca2', // LUCRO de 45.499999999999886
+    institutionId: 'c1daef5f-4bd0-4616-bb62-794e9b5d8ca2', // EARNING 45.499999999999886
     type: TRANSACTION_TYPE.SELL,
     date: new Date('2019-08-01T00:00:00'),
     category: TRANSACTION_CATEGORY.TRADE,
     ticketSymbol: 'AAPL',
     quantity: 50,
     unityPrice: 21.1,
-    totalCost: 1055.0, // restando 50 com custo total de 964, pm de 19.28
+    totalCost: 1055.0, // Holding 50 shares with total cost of 964 and pm of 19.28
   },
   {
     institutionId: 'c1daef5f-4bd0-4616-bb62-794e9b5d8ca2',
@@ -50,9 +50,9 @@ export const transactions = [
     date: new Date('2019-09-01T00:00:00'),
     category: TRANSACTION_CATEGORY.TRADE,
     ticketSymbol: 'GOOG',
-    quantity: 1500, // 1700
-    unityPrice: 12.3, // 12.355294117647059
-    totalCost: 18450.0, // 21004
+    quantity: 1500,
+    unityPrice: 12.3,
+    totalCost: 18450.0, // Holding 1700 shares with total cost of 21004 and pm of 12.355294117647059
   },
   {
     institutionId: 'c1daef5f-4bd0-4616-bb62-794e9b5d8ca2',
@@ -62,7 +62,7 @@ export const transactions = [
     ticketSymbol: 'AAPL',
     quantity: 1500,
     unityPrice: 12.3,
-    totalCost: 18450.0, // restando 1550 com custo total de 19414, pm de 12.52516129032258
+    totalCost: 18450.0, // Holding 1550 shares with total cost of 19414 and pm of 12.52516129032258
   },
   {
     institutionId: 'c1daef5f-4bd0-4616-bb62-794e9b5d8ca2',
@@ -82,37 +82,37 @@ export const transactions = [
     ticketSymbol: 'AAPL',
     quantity: 10,
     unityPrice: 12.3,
-    totalCost: 123, // restando 1560 com custo total de 19537, pm de 12.523717948717948
+    totalCost: 123, // Holding 1560 shares with total cost of 19537 and pm of 12.523717948717948
   },
   {
-    institutionId: 'c1daef5f-4bd0-4616-bb62-794e9b5d8ca2', // lucro 9.762820512820525
+    institutionId: 'c1daef5f-4bd0-4616-bb62-794e9b5d8ca2', // EARNING 9.762820512820525
     type: TRANSACTION_TYPE.SELL,
     date: new Date('2019-09-11T00:00:00'),
-    category: TRANSACTION_CATEGORY.TRADE, // ERRO
+    category: TRANSACTION_CATEGORY.TRADE,
     ticketSymbol: 'AAPL',
     quantity: 10,
     unityPrice: 13.5,
-    totalCost: 135, // restando 1550 com custo total de 19402, pm de 12.51741935483871
+    totalCost: 135, // Holding 1550 shares with total cost of 19402 and pm of 12.51741935483871
   },
   {
-    institutionId: 'c1daef5f-4bd0-4616-bb62-794e9b5d8ca2', // Perca -3776.1290322580644
+    institutionId: 'c1daef5f-4bd0-4616-bb62-794e9b5d8ca2', // LOSS -3776.1290322580644
     type: TRANSACTION_TYPE.SELL,
     date: new Date('2019-10-05T00:00:00'),
     category: TRANSACTION_CATEGORY.TRADE,
     ticketSymbol: 'AAPL',
     quantity: 1500,
     unityPrice: 10,
-    totalCost: 15000, // restando 50 com custo total de 4402, pm de 88.04
+    totalCost: 15000, // Holding 50 shares with total cost of 4402 and pm of 88.04
   },
   {
-    institutionId: 'c1daef5f-4bd0-4616-bb62-794e9b5d8ca2', // lucro 110
+    institutionId: 'c1daef5f-4bd0-4616-bb62-794e9b5d8ca2', // EARNING 110
     type: TRANSACTION_TYPE.SELL,
     date: new Date('2019-11-15T00:00:00'),
     category: TRANSACTION_CATEGORY.TRADE,
     ticketSymbol: 'GOOG',
     quantity: 1700,
     unityPrice: 12.42,
-    totalCost: 21114, // restando 0 com custo total de 0, pm de 0
+    totalCost: 21114, // Holding 0 shares with total cost of 0 and pm of 0
   },
   {
     institutionId: 'c1daef5f-4bd0-4616-bb62-794e9b5d8ca2',
@@ -122,7 +122,7 @@ export const transactions = [
     ticketSymbol: 'AAPL',
     quantity: 10,
     unityPrice: 0,
-    totalCost: 0, // restando 60 com custo total de 4402, pm de 73.36666666666666
+    totalCost: 0, // Holding 60 shares with total cost of 4402 and pm of 73.36666666666666
   },
   {
     institutionId: 'c1daef5f-4bd0-4616-bb62-794e9b5d8ca2',
@@ -132,17 +132,17 @@ export const transactions = [
     ticketSymbol: 'AAPL',
     quantity: 100,
     unityPrice: 0,
-    totalCost: 0, // restando 160 com custo total de 4402, pm de 27.5125
+    totalCost: 0, // Holding 160 shares with total cost of 4402 and pm of 27.5125
   },
   {
-    institutionId: 'c1daef5f-4bd0-4616-bb62-794e9b5d8ca2', // perca de 1202
+    institutionId: 'c1daef5f-4bd0-4616-bb62-794e9b5d8ca2', // LOSS 1202
     type: TRANSACTION_TYPE.SELL,
     date: new Date('2020-01-02T00:00:00'),
     category: TRANSACTION_CATEGORY.TRADE,
     ticketSymbol: 'AAPL',
     quantity: 160,
     unityPrice: 20,
-    totalCost: 3200, // restando 0 com custo total de 0, pm de 0
+    totalCost: 3200, // Holding 0 shares with total cost of 0 and pm of 0
   },
   {
     institutionId: 'c1daef5f-4bd0-4616-bb62-794e9b5d8ca2',
@@ -154,9 +154,4 @@ export const transactions = [
     unityPrice: 10,
     totalCost: 1000,
   },
-
-  // custo_total_novo = custo_total - custo_transaction
-  // qtd = quantitade - quantidade_nova
-  // p.m = custo_total_novo / qtd
-  // lucroPerda = (custo_transaction) - (quantidade_nova * pm_antigo)
 ];
