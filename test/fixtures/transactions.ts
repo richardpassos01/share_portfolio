77f40152393a5012d5eaed4344faf9b4992db47d
Richard <richard.h.passos@hotmail.com>
2023-10-06 22:01:00 -0300

    refactor case tests descriptions

--- test/fixtures/transactions.ts
+++ test/fixtures/transactions.ts
@@ -27,14 +27,14 @@ export const transactions: TransactionParams[] = [
     totalCost: 1254.0,
   },
   {
-    institutionId: institution.id, // EARNING 45.499999999999886
+    institutionId: institution.id,
     type: TRANSACTION_TYPE.SELL,
     date: new Date('2019-08-01T00:00:00'),
     category: TRANSACTION_CATEGORY.TRADE,
     ticketSymbol: 'AAPL',
     quantity: 50,
     unityPrice: 21.1,
-    totalCost: 1055.0, // Holding 50 shares with total cost of 964 and pm of 19.28
+    totalCost: 1055.0,
   },
   {
     institutionId: institution.id,
@@ -54,7 +54,7 @@ export const transactions: TransactionParams[] = [
     ticketSymbol: 'GOOG',
     quantity: 1500,
     unityPrice: 12.3,
-    totalCost: 18450.0, // Holding 1700 shares with total cost of 21004 and pm of 12.355294117647059
+    totalCost: 18450.0,
   },
   {
     institutionId: institution.id,
@@ -64,7 +64,7 @@ export const transactions: TransactionParams[] = [
     ticketSymbol: 'AAPL',
     quantity: 1500,
     unityPrice: 12.3,
-    totalCost: 18450.0, // Holding 1550 shares with total cost of 19414 and pm of 12.52516129032258
+    totalCost: 18450.0,
   },
   {
     institutionId: institution.id,
@@ -84,37 +84,37 @@ export const transactions: TransactionParams[] = [
     ticketSymbol: 'AAPL',
     quantity: 10,
     unityPrice: 12.3,
-    totalCost: 123, // Holding 1560 shares with total cost of 19537 and pm of 12.523717948717948
+    totalCost: 123,
   },
   {
-    institutionId: institution.id, // EARNING 9.762820512820525
+    institutionId: institution.id,
     type: TRANSACTION_TYPE.SELL,
     date: new Date('2019-09-11T00:00:00'),
     category: TRANSACTION_CATEGORY.TRADE,
     ticketSymbol: 'AAPL',
     quantity: 10,
     unityPrice: 13.5,
-    totalCost: 135, // Holding 1550 shares with total cost of 19402 and pm of 12.51741935483871
+    totalCost: 135,
   },
   {
-    institutionId: institution.id, // LOSS -3776.1290322580644
+    institutionId: institution.id,
     type: TRANSACTION_TYPE.SELL,
     date: new Date('2019-10-05T00:00:00'),
     category: TRANSACTION_CATEGORY.TRADE,
     ticketSymbol: 'AAPL',
     quantity: 1500,
     unityPrice: 10,
-    totalCost: 15000, // Holding 50 shares with total cost of 4402 and pm of 88.04
+    totalCost: 15000,
   },
   {
-    institutionId: institution.id, // EARNING 110
+    institutionId: institution.id,
     type: TRANSACTION_TYPE.SELL,
     date: new Date('2019-11-15T00:00:00'),
     category: TRANSACTION_CATEGORY.TRADE,
     ticketSymbol: 'GOOG',
     quantity: 1700,
     unityPrice: 12.42,
-    totalCost: 21114, // Holding 0 shares with total cost of 0 and pm of 0
+    totalCost: 21114,
   },
   {
     institutionId: institution.id,
@@ -124,7 +124,7 @@ export const transactions: TransactionParams[] = [
     ticketSymbol: 'AAPL',
     quantity: 10,
     unityPrice: 0,
-    totalCost: 0, // Holding 60 shares with total cost of 4402 and pm of 73.36666666666666
+    totalCost: 0,
   },
   {
     institutionId: institution.id,
@@ -134,17 +134,17 @@ export const transactions: TransactionParams[] = [
     ticketSymbol: 'AAPL',
     quantity: 100,
     unityPrice: 0,
-    totalCost: 0, // Holding 160 shares with total cost of 4402 and pm of 27.5125
+    totalCost: 0,
   },
   {
-    institutionId: institution.id, // LOSS 1202
+    institutionId: institution.id,
     type: TRANSACTION_TYPE.SELL,
     date: new Date('2020-01-02T00:00:00'),
     category: TRANSACTION_CATEGORY.TRADE,
     ticketSymbol: 'AAPL',
     quantity: 160,
     unityPrice: 20,
-    totalCost: 3200, // Holding 0 shares with total cost of 0 and pm of 0
+    totalCost: 3200,
   },
   {
     institutionId: institution.id,
@@ -164,10 +164,10 @@ export const transactions: TransactionParams[] = [
     ticketSymbol: 'TSLA',
     quantity: 100,
     unityPrice: 12.93,
-    totalCost: 1293, // Holding 200 shares with total cost of 2293 and pm of 11.465
+    totalCost: 1293,
   },
   {
-    institutionId: institution.id, // EARNING 9988.535
+    institutionId: institution.id,
     type: TRANSACTION_TYPE.SELL,
     date: new Date('2021-02-01T00:00:00'),
     category: TRANSACTION_CATEGORY.TRADE,
@@ -177,7 +177,7 @@ export const transactions: TransactionParams[] = [
     totalCost: 10000,
   },
   {
-    institutionId: institution.id, // EARNING 1991990
+    institutionId: institution.id,
     type: TRANSACTION_TYPE.SELL,
     date: new Date('2021-02-02T00:00:00'),
     category: TRANSACTION_CATEGORY.TRADE,
